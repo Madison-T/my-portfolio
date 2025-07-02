3f9a633befadd2f26175e18ecfcb60409cea2bce
Madison-T <madisonlilah@gmail.com>
2025-07-02 16:20:04 +1200

    Updated css for logic mobile compatibility

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,10 @@
 export default function Navbar() {
   return (
-    <header className="navbar">
-      <div className="font-bold text-[24px] text-[color:var(--primary-text)]">Madison</div>
-      <nav>
+    <header className="navbar flex flex-col md:flex-row items-center justify-between text-center md:textleft">
+      <div className="font-bold tet-[24px] text-[color:var(--primary-text)] mb-2 md:mb-0">
+        Madison
+      </div>
+      <nav className="flex flex-col md:flex-row gap-2 md:gap-4">
         <a href="#about">About</a>
         <a href="#projects">Projects</a>
         <a href="#contact">Contact</a>
